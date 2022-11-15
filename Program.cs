d6054c33d088e6eb5d9a230c955c4bcd5c7d3659
SelenaMendia <103127457+SelenaMendia@users.noreply.github.com>
2022-11-14 23:03:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _3_10_programacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arreglosNumericos = {0, 18, 2, 43, 24, 56, 63, 78, 8, 99};
+            int valor;
+            Console.WriteLine("Ingrese un valor: ");
+            valor= Convert.ToInt32(Console.ReadLine());
+
+            buscarEnArreglo(arreglosNumericos, valor);
+
+            Console.WriteLine("Presione una tecla para salir...");
+            Console.ReadKey();
+        }
+
+        private static void buscarEnArreglo(int[] numeros, int buscar)
+        {
+            bool encontrado = false;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] == buscar)
+                {
+                    encontrado = true;
+                }
+            }
+            Console.WriteLine(encontrado == true ? "El valor fue encontrado " : "El valor NO fue encontrado");
+        }
+    }
+}
